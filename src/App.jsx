3f01093c9a63b59810a4c75c36bfd922e180b097
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-26 13:22:57 +0530

    2nd attempt to test links connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePageNavbar from './components/HomePageNavbar';
 
 function App() {
   return (
     <Router>
       <div className="h-screen" style={{ backgroundColor: 'rgba(0,0,0,0.95)' }}>
-        <Routes>
-          {/* Route for the homepage */}
-          <Route
-            path="/"
-            element={
-              <>
-                <HomePageNavbar />
-                {/* You can add more content here for the homepage */}
-              </>
-            }
-          />
+        <HomePageNavbar />
 
-          {/* Other routes, without the navbar */}
+        <nav>
+          {/* Example of using Link component for navigation */}
+          <ul>
+            <li>
+              <Link to="/" className="text-white">Home</Link>
+            </li>
+            <li>
+              <Link to="/about" className="text-white">About</Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-white">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          {/* Routes will render based on the current URL */}
+          <Route path="/" element={<div>Home Page Content</div>} />
           <Route path="/about" element={<div>About Page</div>} />
           <Route path="/contact" element={<div>Contact Page</div>} />
-          {/* Add more routes as needed */}
         </Routes>
       </div>
     </Router>
