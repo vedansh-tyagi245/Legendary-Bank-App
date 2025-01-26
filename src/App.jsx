726285da52f9efa48df889d70dbe9184b0382dec
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-26 13:20:28 +0530

    added links to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import HomePageNavbar from './components/HomePageNavbar'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePageNavbar from './components/HomePageNavbar';
 
 function App() {
   return (
-    <div className="h-screen" style={{ backgroundColor: 'rgba(0,0,0,0.95)' }}>
-      <HomePageNavbar />
-    </div>
-  )
+    <Router>
+      <div className="h-screen" style={{ backgroundColor: 'rgba(0,0,0,0.95)' }}>
+        <Routes>
+          {/* Route for the homepage */}
+          <Route
+            path="/"
+            element={
+              <>
+                <HomePageNavbar />
+                {/* You can add more content here for the homepage */}
+              </>
+            }
+          />
+
+          {/* Other routes, without the navbar */}
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
