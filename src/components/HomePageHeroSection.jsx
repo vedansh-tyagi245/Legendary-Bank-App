3c39f3f05cf30db185588937f2200e6bbf4018ec
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-29 12:13:16 +0530

    uploaded the dancing sticker on the registration page

--- src/components/HomePageHeroSection.jsx
+++ src/components/HomePageHeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Robot from "../assets/robot.gif"
+import { Link } from "react-router-dom";
 
 function HomePageHeroSection() {
     return (
@@ -12,8 +13,8 @@ function HomePageHeroSection() {
                     <p className="text-lg text-gray-100 font-mono lg:text-start text-center">Experience seamless banking solutions with personalized services and cutting-edge technology. Your trust, our commitment.</p>
                     <br />
                     <div className="flex lg:text-start lg:justify-start justify-center lg:gap-4 gap-4">
-                        <button className="bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 h-10 w-36">Open an Account</button>
-                        <button className="bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition duration-300 h-10 w-36">Learn More</button>
+                        <Link to={'/register'} className="bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 h-10 w-[140px] text-center" style={{paddingTop:'9px'}}>Open an Account</Link>
+                        <Link to={'/learnMore'} className="bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition duration-300 h-10 w-[140px] text-center" style={{paddingTop:'9px'}}>Learn More</Link>
                     </div>
                 </div>
 
