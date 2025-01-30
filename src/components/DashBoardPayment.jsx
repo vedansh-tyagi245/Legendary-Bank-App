ed2eb72252a252d2e3d747f27e67eca749436741
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-30 22:43:31 +0530

    again

--- src/components/DashBoardPayment.jsx
+++ src/components/DashBoardPayment.jsx
@@ -7,8 +7,8 @@ function DashBoardPayment() {
                 <div className="p-4 mt-20">
                     <form class="max-w-sm mx-auto border-2 rounded-lg border-dashed border-gray-700  m-10 p-10">
                         <div class="mb-5">
-                            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                            <label for="phone" class="block mb-2 text-sm font-medium text-white">Your email</label>
+                            <input type="text" id="phone" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@flowbite.com" required />
                         </div>
                         <div class="mb-5">
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
