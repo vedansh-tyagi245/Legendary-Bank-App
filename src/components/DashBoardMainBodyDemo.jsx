9b24d01d384b27941d4500f3422a34d1da1aee1f
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-30 20:55:32 +0530

    1st

---
+++ src/components/DashBoardMainBodyDemo.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+
+function DashBoardMainBodyDemo() {
+    return (
+        <div>
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 border-2 border-dashed rounded-lg border-gray-700 mt-14">
+                    <div className="grid grid-cols-3 gap-4 mb-4">
+                        <div className="flex items-center justify-center h-24 rounded-sm bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded-sm bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded-sm bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-800">
+                        <p className="text-2xl text-gray-500">
+                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                            </svg>
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-800">
+                        <p className="text-2xl text-gray-500">
+                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                            </svg>
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-center rounded-sm h-28 bg-gray-800">
+                            <p className="text-2xl text-gray-500">
+                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                </svg>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DashBoardMainBodyDemo
